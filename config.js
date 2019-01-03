8618099e1433b304d33c00067328002a200bae02
Neexona <37214268+Neexona@users.noreply.github.com>
2019-01-03 15:44:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NTI0MjUwOTI5NTA4MjUzNzE3.Dw7muA.ZtHlwbGiN6TKKF-2u6mVcRmFr4w';
+exports.TOKEN = process.env.TOKEN;
 
 exports.PREFIX = 's!';
 
-exports.GOOGLE_API_KEY = 'AIzaSyCjGRSdN0Axpf3d_fq0SYaTpEhcrN9u8-0';
+exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
